9d253ca4087ccca2bb45d2786bdf8aba3a97eafc
Yozmor <149872259+Yozmor@users.noreply.github.com>
2026-04-02 19:22:33 +0300

    Update NetworkReport.ps1

--- network-report/NetworkReport.ps1
+++ network-report/NetworkReport.ps1
@@ -1,6 +1,6 @@
 # NetworkReport.ps1 - pure ASCII, no Unicode, no WHOIS, fast tracert
 # Версия скрипта – меняй вручную при каждом значимом обновлении
-$scriptVersion = "3.2"
+$scriptVersion = "3.3"
 
 
 $maxHops = 30
